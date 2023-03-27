94a41d6c8468bdc7782eb2bfdfe268ae9ccbc7ee
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-03-27 10:29:19 +0530

    Update input_urls.py

--- input_urls.py
+++ input_urls.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
 def take_input():
-  
+  df = pd.read_csv('/urls/urls.csv')
+  print("write 'end' for end of urls\n")
+  while(True):
+    url = input()
+    if url == 'end':
+      break
+      
