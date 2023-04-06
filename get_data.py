b63ca01891635f1bee06cc2968ac2ea923bbbc3d
Rahul verma <100432854+NME-rahul@users.noreply.github.com>
2023-04-06 12:32:16 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -4,62 +4,32 @@ from bs4 import BeautifulSoup
 
 Error0 = 'Error: in internet connection.'
 
+
+def control_tag(word, tag, soup):
+  with open('data.txt', 'w') as fp:
+    sentence = ''; start = 0; end = 0;
+    for i in soup.findAll(tag):
+      sentence = i.text
+      print(type(sentence))
+      fp.write(sentence)
+      if word in sentence:
+        print(word)
+
 def collect_data(keywords):
-  df = pd.read_csv('/data/urls.csv')
+  df = pd.read_csv('urls/urls.csv')
   
   for i in range(len(df)):
     try:
-      res = rq.get(df['url'][i])
+      res = rq.get(df['urls'][i])
     except:
       print(Error0)
     soup = BeautifulSoup(res.content, 'html.parser')
+    
     #based on class
-    #based text keywords
-    text = []
-    start = 0; end = 0;
-    fp = open('data.txt', 'w')
+    #based on keyword
+    tags = ['title', 'h1', 'h2', 'h3', 'p']
+    
     for word in keywords:
-      fp.write(word + '\n')
-      for i in soup.findAll('p'):
-        text.append(i)
-        if word in text or (start == 0 or start > 0):
-          fp.write(i)
-          start = start + 1
-          #write in data file, write next 50 word and until '.'
-        if start == 50:
-          start = 0
-          break
-      text.clear()
-      for i in soup.findAll('h1'):
-        text.append(i)
-        if word in text or (start == 0 or start > 0):
-          fp.write(i)
-          start = start + 1
-          #write in data file, write next 50 word and until '.'
-        if start == 50:
-          start = 0
-          break
-          #write in data file
-      text.clear()
-      for i in soup.findAll('h2'):
-        text.append(i)
-        if word in text or (start == 0 or start > 0):
-          fp.write(i)
-          start = start + 1
-          #write in data file, write next 50 word and until '.'
-        if start == 50:
-          start = 0
-          break
-          #write in data file
-      text.clear()
-      for i in soup.findAll('h3'):
-        text.append(i)
-        if word in text or (start == 0 or start > 0):
-          fp.write(i)
-          start = start + 1
-          #write in data file, write next 50 word and until '.'
-        if start == 50:
-          start = 0
-          break
-          #write in data file
-      text.clear()
+      for tag in tags:
+        print(word, tag)
+        control_tag(word, tag, soup)
